edf8247a2a7e6f95f8d3a00099cd98a8af73e1da
carlosamawork <carlos@ama.work>
2026-02-16 18:32:36 +0100

    First admin finished

--- sanity/desk/orderProductStructure.ts
+++ sanity/desk/orderProductStructure.ts
@@ -3,7 +3,6 @@ import defineStructure from '../utils/defineStructure'
 import {DocumentsIcon} from '@sanity/icons'
 import {orderableDocumentListDeskItem} from '@sanity/orderable-document-list'
 
-
 export default defineStructure((S, context) =>
   S.listItem()
     .title('Ordenar Productos')
